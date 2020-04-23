afa2ee74291075ffed6eb9bdc1239a102e96521f
pkesha <parham.keshavarzi7117@gmail.com>
2020-04-23 15:16:45 -0500

    "Defanging an IP address"

--- src/com/keshavarziparham/DefangingAnIPAddress.java
+++ src/com/keshavarziparham/DefangingAnIPAddress.java
@@ -1,7 +1,6 @@
 package com.keshavarziparham;
 
-public class DefangingAnIPAddress {
-    /*
+/*
     Given a valid (IPv4) IP address, return a defanged version of that IP address.
     A defanged IP address replaces every period "." with "[.]".
 
@@ -9,12 +8,22 @@ public class DefangingAnIPAddress {
     The given address is a valid IPv4 address.
      */
 
+public class DefangingAnIPAddress {
 
     public static void main(String[] args) {
         String address = "1.11.1.1";
         System.out.println(defrangIPaddr(address));
     }
 
+    public static String defrangIPaddr1(String address){
+        int i = 0;
+        while (address.indexOf(i) != -1){
+            if (address.indexOf(i) == '.'){
+                //address.
+            }
+        }
+        return null;
+    }
 
     public static String defrangIPaddr(String address) {
         return address.replace(".", "[.]");
