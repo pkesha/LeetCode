6ec13a54993679ce8bcc8016a7f31f14640923fb
pkesha <parham.keshavarzi7117@gmail.com>
2020-08-26 18:07:11 -0500

    "Jewels and Stones Challenge"

--- src/com/parhamkeshavarzi/DefangingAnIPAddress.java
+++ src/com/parhamkeshavarzi/DefangingAnIPAddress.java
@@ -1,4 +1,4 @@
-package com.keshavarziparham;
+package com.parhamkeshavarzi;
 
 /*
     Given a valid (IPv4) IP address, return a defanged version of that IP address.
--- src/com/parhamkeshavarzi/JewelsAndStones.java
+++ src/com/parhamkeshavarzi/JewelsAndStones.java
@@ -1,4 +1,4 @@
-package com.keshavarziparham;
+package com.parhamkeshavarzi;
 /*
 You're given strings J representing the types of stones that are jewels, and S representing the stones you have.
 Each character in S is a type of stone you have.
@@ -20,7 +20,6 @@ Note:
  */
 
 import java.util.HashSet;
-import java.util.List;
 
 public class JewelsAndStones {
     public static void main(String[] args) {
