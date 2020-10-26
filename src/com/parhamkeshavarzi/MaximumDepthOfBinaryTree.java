a6dea07b4cbe5f1f9145276a8db22d3a8dd6c1a2
pkesha <parham.keshavarzi7117@gmail.com>
2020-10-25 21:13:14 -0500

    "Single Number"

--- src/com/parhamkeshavarzi/MaximumDepthOfBinaryTree.java
+++ src/com/parhamkeshavarzi/MaximumDepthOfBinaryTree.java
@@ -51,13 +51,15 @@ public class MaximumDepthOfBinaryTree {
             return 0;
         }
 
-        int left = maxDepth(root.left());
+        //Had to comment since left and right methods didn't exist for this java file.
+        /*int left = maxDepth(root.left());
         int right = maxDepth(root.right());
 
         if (left > right) {
             return left + 1;
         } else {
             return right + 1;
-        }
+        }*/
+        return 1;
     }
 }
